79dc8d2afb05d47cd2a60ab910a0cc89fb0d10b8
robot-ux <robot.ux@protonmail.com>
2025-03-13 20:39:13 +0800

    chore: Trigger create-bnb release

--- packages/.releaserc.js
+++ packages/.releaserc.js
@@ -1,9 +1,11 @@
 const args = process.argv.slice(2);
 const pkgArgs = args.find((it) => it.startsWith("--package=")) || [];
 const pkgName = pkgArgs.split("=")[1] || "no-release";
-const headerPattern = new RegExp(`^(\\w+)\\(${pkgName}\\):\\s+(.*)$`);
+
+const headerPattern = new RegExp(`^(\\w+)\\((${pkgName})\\):\\s+(.*)$`);
 
 module.exports = {
+  tagFormat: pkgName + "-v${version}",
   branches: ["main"],
   plugins: [
     [
@@ -18,13 +20,42 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
+        writerOpts: {
+          headerPartial: `## 🚀 ${pkgName}` + "-v{{version}}({{date}})\n\n",
+          transform: (_commit, context) => {
+            const commit = { ..._commit };
+            if (!commit.scope) return;
+
+            // Add PR link if available
+            if (commit.pullRequest && commit.pullRequest.number) {
+              commit.pr = `([#${commit.pullRequest.number}](${context.repository}/pull/${commit.pullRequest.number}))`;
+            }
+
+            // Generate short hash with link
+            if (commit.hash) {
+              commit.shortHash = `([\`${commit.hash.substring(0, 7)}\`](${
+                context.repository
+              }/commit/${commit.hash}))`;
+            }
+
+            if (commit.type === "feat") {
+              commit.type = "✨ Features";
+            } else if (commit.type === "fix") {
+              commit.type = "🐛 Bug Fixes";
+            } else {
+              commit.type = "🛠️ Other Changes";
+            }
+
+            return commit;
+          },
+
+          // Define the commit message format in release notes
+          commitPartial: "- **{{subject}}**{{pr}} {{shortHash}}\n",
+        },
         parserOpts: {
           headerPattern,
         },
       },
     ],
-    // ["@semantic-release/npm", { pkgRoot: "packages/${package}" }],
-    // ["@semantic-release/github", { assets: "packages/${package}/dist/**" }],
-    // ["@semantic-release/git", { assets: ["packages/${package}/package.json"] }],
   ],
 };
